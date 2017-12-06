94833abf8595543bdff3f924f8d31a1d557a15cc
kjh <spieler3001@gmail.com>
2017-12-06 06:23:04 +0100

    day06

---
+++ src/main/kotlin/Day06.kt
@@ -0,0 +1,45 @@
+import java.io.File
+
+fun main(args: Array<String>) {
+    val listOfInts = mutableListOf<Int>()
+    File("resources/day06.txt").bufferedReader().useLines {lines ->
+        lines.forEach {line ->
+            line.split("\t").forEach { word ->
+                listOfInts.add(word.toInt())
+            }
+
+        }
+    }
+
+    println(solve(listOfInts))
+}
+
+fun solve(thing: MutableList<Int>) : Int {
+    val knownConfigurations = mutableSetOf<List<Int>>()
+    var counter = 0
+    while (!knownConfigurations.contains(thing)) {
+        knownConfigurations.add(List(thing.size) {i-> thing[i] + 0} )
+        counter++
+        var biggestIndex = 0
+
+        var biggestNum = 0
+        thing.forEachIndexed {index, num ->
+            if (num > biggestNum) {
+                biggestNum = num
+                biggestIndex = index
+            }
+        }
+        thing[biggestIndex] = 0
+        while (biggestNum > 0) {
+            biggestNum--
+            biggestIndex = (biggestIndex + 1) % thing.size
+            thing[biggestIndex]++
+        }
+    }
+    knownConfigurations.forEachIndexed {index, list ->
+        if (list == thing) {
+            return (knownConfigurations.size - index)
+        }
+    }
+    return counter
+}
