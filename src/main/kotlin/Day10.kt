957878840d6fa1117daed242752cad08c1d959df
kjh <spieler3001@gmail.com>
2017-12-10 06:24:35 +0100

    Day 10

---
+++ src/main/kotlin/Day10.kt
@@ -0,0 +1,52 @@
+import usbpc.aoc.inputgetter.AdventOfCode
+
+class Day10(override val adventOfCode: AdventOfCode) : Day {
+    val input = adventOfCode.getInput(2017, 10)
+    var part1: String? = null
+    var part2: String? = null
+
+    override fun part1(): String {
+        val modified = List(256) {it}.knot(input.split(',').map {it.toInt()}, 1)
+        return (modified[0] * modified[1]).toString()
+    }
+
+    override fun part2(): String {
+        val chunks = List(256) {it}
+                .knot(
+                        input.toByteArray().map {it.toInt()}.toMutableList().apply {addAll(listOf(17, 31, 73, 47, 23))},
+                        64)
+                .chunked(16)
+        val output = StringBuilder()
+        chunks.forEach {chunk ->
+            var xor = 0
+            chunk.forEach {current ->
+                xor = xor xor current
+            }
+            if (xor.toString(16).length < 2) {
+                output.append("0")
+            }
+            output.append(xor.toString(16))
+        }
+        return output.toString()
+    }
+
+    private fun List<Int>.knot(lengths: List<Int>, rounds: Int): List<Int> {
+        val list = this.toMutableList()
+        var currentPosition = 0
+        var skipSize = 0
+        for (counter in 1..rounds) {
+            for (number in lengths) {
+                if (number > list.size)
+                    continue
+                val reversedPart = List(number) {list[(currentPosition + it) % list.size]}.reversed()
+                reversedPart.forEachIndexed {index, thing ->
+                    list[(currentPosition + index) % list.size] = thing
+                }
+                //println("$list skip: $skipSize length: $number pos: $currentPosition")
+                currentPosition = (currentPosition + number + skipSize) % list.size
+                skipSize++
+            }
+        }
+        return list
+    }
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,10 +5,67 @@ fun main(args: Array<String>) {
         println("Please specify session id!")
         return
     }
-    val adventOfcode = AdventOfCode(args[0])
-    val day = Day09(adventOfcode)
+    val adventOfCode = AdventOfCode(args[0])
+    val day = Day10(adventOfCode)
     println("""
         |Part 1: ${day.part1()}
         |Part 2: ${day.part2()}
     """.trimMargin())
+}
+
+class Y2015D11(override val adventOfCode: AdventOfCode) : Day {
+    var part1: String? = null
+    var part2: String? = null
+    override fun part1(): String {
+        if (part1 == null) {
+            solve()
+        }
+        return part1.toString()
+    }
+
+    override fun part2(): String {
+        if (part2 == null) {
+            solve()
+        }
+        return part2.toString()
+    }
+
+    private fun solve() {
+        val oldPw = adventOfCode.getInput(2015, 11)
+        var newPw = oldPw
+        while (!newPw.isValidPassword()) {
+            newPw = newPw.inc()
+        }
+        part1 = newPw
+        newPw = newPw.inc()
+        while (!newPw.isValidPassword()) {
+            newPw = newPw.inc()
+        }
+        part2 = newPw
+    }
+    private fun String.inc(): String {
+        var tmp = this.reversed().toCharArray()
+        for (index in 0 until tmp.size) {
+            tmp[index]++
+            if (tmp[index] == 'z' + 1) {
+                tmp[index] = 'a'
+            } else {
+                break
+            }
+        }
+        return String(tmp).reversed()
+    }
+    private val regex = Regex("([a-z])\\1[a-z]*([a-z])\\2")
+    private fun String.isValidPassword(): Boolean {
+        if (this.contains("(i|o|l)"))
+            return false
+        if (!regex.containsMatchIn(this))
+            return false
+        if (this.withIndex()
+                .drop(2).none {(index, char) ->
+            this[index - 2] == char - 2 && this[index - 1] == char - 1
+        })
+            return false
+        return true
+    }
 }
