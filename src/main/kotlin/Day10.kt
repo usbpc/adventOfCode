31bc3e819707dd557143d31ed8b09cc836bab410
kjh <spieler3001@gmail.com>
2017-12-10 10:48:47 +0100

    Reversing in place now

--- src/main/kotlin/Day10.kt
+++ src/main/kotlin/Day10.kt
@@ -4,15 +4,16 @@ class Day10(override val adventOfCode: AdventOfCode) : Day {
     val input = adventOfCode.getInput(2017, 10)
 
     override fun part1(): String {
-        val modified = List(256) {it}.knot(input.split(',').map {it.toInt()}, 1)
+        val modified = IntArray(256) {it}.knot(input.split(',').map {it.toInt()}, 1)
         return (modified[0] * modified[1]).toString()
     }
 
     override fun part2(): String {
         val lengths = input.toByteArray().map {it.toInt()}.toMutableList()
         lengths.addAll(listOf(17, 31, 73, 47, 23))
-        val chunks = List(256) {it}
+        val chunks = IntArray(256) {it}
                 .knot(lengths, 64)
+                .asIterable()
                 .chunked(16)
         val output = StringBuilder()
         chunks.forEach {chunk ->
@@ -20,23 +21,23 @@ class Day10(override val adventOfCode: AdventOfCode) : Day {
             chunk.forEach {number ->
                 xored = xored xor number
             }
-
             output.append(xored.toString(16).padStart(2, '0'))
         }
         return output.toString()
     }
 
-    private fun List<Int>.knot(lengths: List<Int>, rounds: Int): List<Int> {
-        val list = this.toMutableList()
+    private fun IntArray.knot(lengths: List<Int>, rounds: Int): IntArray {
+        val list = this.copyOf()
         var currentPosition = 0
         var skipSize = 0
-        for (counter in 1..rounds) {
+        var tmp: Int
+        repeat (rounds) {
             for (number in lengths) {
                 if (number > list.size) continue
-
-                val reversedPart = List(number) {list[(currentPosition + it) % list.size]}.reversed()
-                reversedPart.forEachIndexed {index, thing ->
-                    list[(currentPosition + index) % list.size] = thing
+                for (curr in 0 until number/2) {
+                    tmp = list[(currentPosition + curr) % list.size]
+                    list[(currentPosition + curr) % list.size] = list[(currentPosition + number - 1 - curr) % list.size]
+                    list[(currentPosition + number - 1 - curr) % list.size] = tmp
                 }
                 currentPosition = (currentPosition + number + skipSize++) % list.size
             }
