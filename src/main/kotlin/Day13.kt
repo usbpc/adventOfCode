8b29b6affa6f542ca0499e5321cf6f716bbfd60b
kjh <spieler3001@gmail.com>
2017-12-13 15:40:24 +0100

    Day 13 done

--- src/main/kotlin/Day13.kt
+++ src/main/kotlin/Day13.kt
@@ -2,26 +2,25 @@ import xyz.usbpc.aoc.Day
 import xyz.usbpc.aoc.inputgetter.AdventOfCode
 
 class Day13(override val adventOfCode: AdventOfCode) : Day {
+    private data class FirewallLayer(val layer: Int, val depth: Int)
     override val day: Int = 13
-    private val sample = """0: 3
-1: 2
-4: 4
-6: 4"""
     private val regex = Regex("(\\d+): (\\d+)")
-    private val input = adventOfCode.getInput(2017, 13).lines().map {
-        regex.find(it)?.groups?.drop(1)?.map {it?.value?.toInt()}?.requireNoNulls()
-    }.requireNoNulls()
+    private val input = adventOfCode.getInput(2017, 13).lines()
+            .map {line ->
+                regex.find(line)?.groups?.drop(1)
+                        ?.map {it?.value?.toInt()}
+                        ?.requireNoNulls()
+            }.requireNoNulls()
+            .map {(layer, depth) -> FirewallLayer(layer, depth)}
 
-    override fun part1(): String =
-            input.filter {it[0] % (2 * (it[1] - 2) + 2) == 0}.fold(0) {acc, crr ->
-                acc + crr[0] * crr[1]
-            }.toString()
+    override fun part1(): String = input
+            .filter {(layer, depth) -> layer % (2 * (depth - 2) + 2) == 0}
+            .fold(0) {acc, (layer, depth) -> acc + layer * depth}
+            .toString()
 
-    override fun part2(): String {
-        var delay = 0
-        while (input.any {(it[0] + delay) % (2 * (it[1] - 2) + 2) == 0}) {
-            delay++
-        }
-        return delay.toString()
-    }
+    override fun part2(): String = generateSequence(0) {it + 1}
+            .first {delay ->
+                input.none {(layer, depth) -> (layer + delay) % (2 * (depth - 2) + 2) == 0}
+            }
+            .toString()
 }
