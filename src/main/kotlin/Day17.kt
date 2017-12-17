06f45c2fbce2bb62610a0e999c637a31b2bf8a48
kjh <spieler3001@gmail.com>
2017-12-17 07:27:18 +0100

    Clean day 17

--- src/main/kotlin/Day17.kt
+++ src/main/kotlin/Day17.kt
@@ -6,34 +6,28 @@ class Day17(override val adventOfCode: AdventOfCode) : Day {
     private val input = adventOfCode.getInput(2017, day).toInt()
 
     override fun part1(): String {
-        var insertedAt = 0
-        var counter = 0
         var currentPos = 0
-        val buffer = mutableListOf<Int>()
-        while (counter <= 2017) {
-            if (currentPos >= buffer.size - 1) {
-                buffer.add(counter++)
+        val buffer = mutableListOf(0)
+        for (counter in 1..2017) {
+            currentPos = (currentPos + input) % buffer.size + 1
+            if (currentPos >= buffer.size) {
+                buffer.add(counter)
             } else {
-                buffer.add(currentPos + 1, counter++)
+                buffer.add(currentPos, counter)
             }
-            insertedAt = currentPos + 1
-            currentPos = (currentPos + 1 + input) % buffer.size
         }
-
-        return buffer[(insertedAt + 1) % buffer.size].toString()
+        return buffer[(currentPos + 1) % buffer.size].toString()
     }
 
     override fun part2(): String {
         var curr = 0
-        var counter = 0
         var currentPos = 0
-        while (counter <= 50000000) {
-            if (currentPos == 0) {
+        for (counter in 1..50000000) {
+            currentPos = (currentPos + input) % counter + 1
+            if (currentPos == 1) {
                 curr = counter
             }
-            currentPos = (currentPos + 1 + input) % ++counter
         }
-
         return curr.toString()
     }
 }
