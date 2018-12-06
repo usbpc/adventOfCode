52ac176d9684f69f1e370e6a1af2d1772a22b7c6
usbpc <spieler3001@googlemail.com>
2018-12-06 06:38:05 +0100

    Day 06 done

--- src/main/kotlin/advent2018/Day05.kt
+++ src/main/kotlin/advent2018/Day05.kt
@@ -3,7 +3,6 @@ package advent2018
 import xyz.usbpc  .aoc.Day
 import xyz.usbpc.aoc.inputgetter.AdventOfCode
 import java.lang.StringBuilder
-import java.util.*
 
 class Day05(override val adventOfCode: AdventOfCode) : Day {
     override val day: Int = 5
@@ -17,15 +16,7 @@ class Day05(override val adventOfCode: AdventOfCode) : Day {
         }
     }
 
-    private fun <E> Stack<E>.properPeek() : E? {
-        return if (this.empty()) {
-            null
-        } else {
-            this.peek()
-        }
-    }
-
-    private fun String.reduce() : String {
+    private fun String.reactPolymer() : String {
         val stack = StringBuilder()
 
         for (c in this) {
@@ -40,18 +31,14 @@ class Day05(override val adventOfCode: AdventOfCode) : Day {
     }
 
     override fun part1(): String {
-        return "" + input.reduce().length
+        return "" + input.reactPolymer().length
     }
 
+    private fun String.removeCaseInsensitive(c: Char) : String =
+            this.replace(Regex("${c.toUpperCase()}|${c.toLowerCase()}"), "")
+
+
     override fun part2(): String {
-        val results = mutableListOf<String>()
-
-        for (c in 'a'..'z') {
-            var string = input.replace("$c", "").replace("${c.toUpperCase()}", "").reduce()
-
-            results.add(string)
-        }
-
-        return "" + results.minBy { it.length }!!.length
+        return "" + ('a'..'z').map { input.removeCaseInsensitive(it).reactPolymer() }.minBy { it.length }!!.length
     }
 }
