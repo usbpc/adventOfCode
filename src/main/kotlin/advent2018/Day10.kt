9d75ddfb265bdafb5c900e7014038718db914d97
usbpc <spieler3001@googlemail.com>
2018-12-10 08:17:47 +0100

    Day 10 with character recognition

--- src/main/kotlin/advent2018/Day10.kt
+++ src/main/kotlin/advent2018/Day10.kt
@@ -2,9 +2,7 @@ package advent2018
 
 import xyz.usbpc.aoc.Day
 import xyz.usbpc.aoc.inputgetter.AdventOfCode
-import java.io.BufferedWriter
 import java.io.File
-import java.io.OutputStreamWriter
 import java.lang.StringBuilder
 
 class Day10(override val adventOfCode: AdventOfCode) : Day {
@@ -25,6 +23,7 @@ class Day10(override val adventOfCode: AdventOfCode) : Day {
             this.y -= v.y
         }
     }
+
     data class Velocity(val x : Long, val y : Long)
 
     fun printGrid(data : List<Pair<Position, Velocity>>) : String {
@@ -40,7 +39,7 @@ class Day10(override val adventOfCode: AdventOfCode) : Day {
                 if (data.filter { it.first == Position(x, y) }.any()) {
                     out.append('#')
                 } else {
-                    out.append('.')
+                    out.append(' ')
                 }
             }
             out.append('\n')
@@ -49,7 +48,30 @@ class Day10(override val adventOfCode: AdventOfCode) : Day {
         return out.toString()
     }
 
+    private fun getLetterMap() : Map<String, Char> {
+        val fileText = File("resources/letters.txt").readText().split(".\n")
+        val bigLetters = fileText.dropLast(1).map { it.dropLast(1) }
+        val letters = fileText.last()
+        return bigLetters.zip(letters.toList()).toMap()
+    }
+
+    fun String.splitLettersToList() : List<String> {
+        val lines = this.lines()
+        val out = mutableListOf<String>()
+        for (i in 0..7) {
+            val builder = StringBuilder()
+            for (line in lines) {
+                builder.append(line.subSequence((6+2)*i..(6+2)*i+5))
+                builder.append('\n')
+            }
+            builder.deleteCharAt(builder.lastIndex)
+            out.add(builder.toString())
+        }
+        return out
+    }
+
     override fun part1(): String {
+        val bigToSmall = getLetterMap()
         val inputCopy = input.map { it.first.copy() to it.second.copy() }
         var distance = Long.MAX_VALUE
         var counter = -1L
@@ -69,7 +91,7 @@ class Day10(override val adventOfCode: AdventOfCode) : Day {
         }
 
         inputCopy.forEach { it.first.sub(it.second) }
-        return  "\n" + printGrid(inputCopy)
+        return  "" + printGrid(inputCopy).splitLettersToList().map { bigToSmall[it] }.requireNoNulls().fold(StringBuilder()) { acc, c -> acc.append(c) }
     }
 
     override fun part2(): String {
@@ -92,4 +114,5 @@ class Day10(override val adventOfCode: AdventOfCode) : Day {
         }
         return "$counter"
     }
+
 }
