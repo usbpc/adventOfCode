4b7b7c6458c6beb5762a406711f4986bd28c554f
usbpc <usbpc@usbpc.xyz>
2018-12-14 13:05:06 +0100

    Day 14 paralel

--- src/main/kotlin/advent2018/Day14.kt
+++ src/main/kotlin/advent2018/Day14.kt
@@ -1,87 +1,110 @@
 package advent2018
 
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.ReceiveChannel
 import xyz.usbpc.aoc.Day
 import xyz.usbpc.aoc.inputgetter.AdventOfCode
 import java.lang.StringBuilder
+import java.util.*
 
 class Day14(override val adventOfCode: AdventOfCode) : Day {
     override val day: Int = 14
     private val input = adventOfCode.getInput(2018, day)
 
-    override fun part1(): String {
-        val recipeNum = input.toInt()
-        var firstElfCur = 0
-        var secondElfCur = 1
-        val recipeScores = mutableListOf(3, 7)
-
-        while (recipeScores.size < recipeNum + 10) {
-            val sum = recipeScores[firstElfCur] + recipeScores[secondElfCur]
+    private fun generateSequence() = sequence {
+        var firstElf = 0
+        var secondElf = 1
+        val scores = mutableListOf(3, 7)
+        yield(3)
+        yield(7)
+        while (true) {
+            val sum = scores[firstElf] + scores[secondElf]
             if (sum >= 10) {
                 val firstDigit = sum / 10
                 val secondDigit = sum % 10
-                recipeScores.add(firstDigit)
-                recipeScores.add(secondDigit)
+                scores.add(firstDigit)
+                yield(firstDigit)
+                scores.add(secondDigit)
+                yield(secondDigit)
             } else {
-                recipeScores.add(sum)
+                scores.add(sum)
+                yield(sum)
             }
 
-            firstElfCur = (firstElfCur + recipeScores[firstElfCur] + 1) % recipeScores.size
-            secondElfCur = (secondElfCur +recipeScores[secondElfCur] + 1) % recipeScores.size
+            firstElf = (firstElf + scores[firstElf] + 1) % scores.size
+            secondElf = (secondElf +scores[secondElf] + 1) % scores.size
+        }
+    }
+
+    private fun CoroutineScope.generateSequence() : ReceiveChannel<Int> {
+        val channel = Channel<Int>(5)
+        launch {
+            try {
+                var firstElf = 0
+                var secondElf = 1
+                val scores = mutableListOf(3, 7)
+                channel.send(3)
+                channel.send(7)
+                while (isActive) {
+                    val sum = scores[firstElf] + scores[secondElf]
+                    if (sum >= 10) {
+                        val firstDigit = sum / 10
+                        val secondDigit = sum % 10
+                        scores.add(firstDigit)
+                        channel.send(firstDigit)
+                        scores.add(secondDigit)
+                        channel.send(secondDigit)
+                    } else {
+                        scores.add(sum)
+                        channel.send(sum)
+                    }
+
+                    firstElf = (firstElf + scores[firstElf] + 1) % scores.size
+                    secondElf = (secondElf +scores[secondElf] + 1) % scores.size
+                }
+            } catch (e: Exception) {
+                println("I got an exception: $e")
+            }
+        }
+        return channel
+    }
+
+    override fun part1(): String = runBlocking(Dispatchers.Default) {
+        val numbers = generateSequence()
+        repeat(input.toInt()) {
+            numbers.receive()
         }
 
         val out = StringBuilder()
 
-        for (i in recipeNum until recipeNum+10) {
-            out.append(recipeScores[i])
+        repeat (10) {
+            out.append(numbers.receive())
         }
 
-        return out.toString()
+        numbers.cancel()
+        return@runBlocking out.toString()
     }
 
-    override fun part2(): String {
-        val inputList = input.map { c -> c - '0'}
-        var firstElfCur = 0
-        var secondElfCur = 1
-        val recipeScores = mutableListOf(3, 7)
+    override fun part2(): String = runBlocking(Dispatchers.Default) {
+        val numbers = generateSequence()
+        val toFind = input.map { c -> c - '0' }
+        var counter = 0
 
-        while (true) {
-            val sum = recipeScores[firstElfCur] + recipeScores[secondElfCur]
-            if (sum >= 10) {
-                val firstDigit = sum / 10
-                val secondDigit = sum % 10
-                recipeScores.add(firstDigit)
-                recipeScores.add(secondDigit)
-            } else {
-                recipeScores.add(sum)
-            }
-
-            if (recipeScores.size >= inputList.size) {
-                if (sum >= 10 && recipeScores.size-1 >= inputList.size) {
-                    var cmp = true
-                    for (i in 0..inputList.lastIndex) {
-                        if (inputList[i] != recipeScores[i+recipeScores.lastIndex-inputList.lastIndex-1]) {
-                            cmp = false
-                            break
-                        }
-                    }
-                    if (cmp) {
-                        return "${recipeScores.size - 1 - inputList.size}"
-                    }
-                }
-                var cmp = true
-                for (i in 0..inputList.lastIndex) {
-                    if (inputList[i] != recipeScores[i+recipeScores.lastIndex-inputList.lastIndex]) {
-                        cmp = false
-                        break
-                    }
-                }
-                if (cmp) {
-                    return "${(recipeScores.size) - inputList.size}"
-                }
-            }
-
-            firstElfCur = (firstElfCur + recipeScores[firstElfCur] + 1) % recipeScores.size
-            secondElfCur = (secondElfCur +recipeScores[secondElfCur] + 1) % recipeScores.size
+        val compare = LinkedList<Int>()
+        repeat(toFind.size) {
+            compare.add(numbers.receive())
+            counter++
         }
+
+        while (!toFind.equals(compare)) {
+            compare.pop()
+            compare.add(numbers.receive())
+            counter++
+        }
+
+        numbers.cancel()
+
+        return@runBlocking "$counter"
     }
 }
