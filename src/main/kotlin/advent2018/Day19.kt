fea58675069c34754a5ff55119c667b6f6d4beee
usbpc <usbpc@usbpc.xyz>
2018-12-19 18:56:44 +0100

    day 19 optimized and automated both parts

--- src/main/kotlin/advent2018/Day19.kt
+++ src/main/kotlin/advent2018/Day19.kt
@@ -4,6 +4,7 @@ import jdk.nashorn.internal.runtime.regexp.joni.constants.OPCode
 import xyz.usbpc.aoc.Day
 import xyz.usbpc.aoc.inputgetter.AdventOfCode
 import java.lang.IllegalStateException
+import kotlin.math.sqrt
 
 class Day19(override val adventOfCode: AdventOfCode) : Day {
     override val day: Int = 19
@@ -52,105 +53,67 @@ class Day19(override val adventOfCode: AdventOfCode) : Day {
 
     private val input = adventOfCode.getInput(2018, 19).lines()
 
-    override fun part1(): String {
-        val registers = IntArray(6)
-        registers[0] = 1
-        val instructionPointer = input.first().extractInts()[0]
-        val program = input.drop(1).map {line ->
-            val numbers = line.extractInts()
-            val op = when(line.substring(0..3)) {
-                "addr" -> Operation.ADDR
-                "addi" -> Operation.ADDI
-                "mulr" -> Operation.MULR
-                "muli" -> Operation.MULI
-                "banr" -> Operation.BANR
-                "bani" -> Operation.BANI
-                "borr" -> Operation.BORR
-                "bori" -> Operation.BORI
-                "setr" -> Operation.SETR
-                "seti" -> Operation.SETI
-                "gtir" -> Operation.GTIR
-                "gtri" -> Operation.GTRI
-                "gtrr" -> Operation.GTRR
-                "eqir" -> Operation.EQIR
-                "eqri" -> Operation.EQRI
-                "eqrr" -> Operation.EQRR
-                else -> throw IllegalStateException("How did we get ${line.substring(0..3)}?")
-            }
-            Instruction(op, numbers[0], numbers[1], numbers[2])
+    private fun List<String>.getInstructions() =
+    this.drop(1).map {line ->
+        val numbers = line.extractInts()
+        val op = when(line.substring(0..3)) {
+            "addr" -> Operation.ADDR
+            "addi" -> Operation.ADDI
+            "mulr" -> Operation.MULR
+            "muli" -> Operation.MULI
+            "banr" -> Operation.BANR
+            "bani" -> Operation.BANI
+            "borr" -> Operation.BORR
+            "bori" -> Operation.BORI
+            "setr" -> Operation.SETR
+            "seti" -> Operation.SETI
+            "gtir" -> Operation.GTIR
+            "gtri" -> Operation.GTRI
+            "gtrr" -> Operation.GTRR
+            "eqir" -> Operation.EQIR
+            "eqri" -> Operation.EQRI
+            "eqrr" -> Operation.EQRR
+            else -> throw IllegalStateException("How did we get ${line.substring(0..3)}?")
         }
-        var oldReg0 = registers[0]
-        while(registers[instructionPointer] >= 0 && registers[instructionPointer] < program.size) {
-            registers.applyOperation(program[registers[instructionPointer]])
-            if (oldReg0 != registers[0]) {
-                println(registers[0])
-                oldReg0 = registers[0]
-            }
-            registers[instructionPointer]++
-        }
+        Instruction(op, numbers[0], numbers[1], numbers[2])
+    }
 
-        return "" + registers[0]
+    override fun part1(): String {
+        val program = input.getInstructions()
+        val toFactor = 2*2*19*11 + (program[21].B*22+program[23].B)
+        return divisorSum(toFactor).toString()
     }
 
 
     override fun part2(): String {
-        /*
-        val program = input.drop(1).map {line ->
-            val numbers = line.extractInts()
-            val op = when(line.substring(0..3)) {
-                "addr" -> Operation.ADDR
-                "addi" -> Operation.ADDI
-                "mulr" -> Operation.MULR
-                "muli" -> Operation.MULI
-                "banr" -> Operation.BANR
-                "bani" -> Operation.BANI
-                "borr" -> Operation.BORR
-                "bori" -> Operation.BORI
-                "setr" -> Operation.SETR
-                "seti" -> Operation.SETI
-                "gtir" -> Operation.GTIR
-                "gtri" -> Operation.GTRI
-                "gtrr" -> Operation.GTRR
-                "eqir" -> Operation.EQIR
-                "eqri" -> Operation.EQRI
-                "eqrr" -> Operation.EQRR
-                else -> throw IllegalStateException("How did we get ${line.substring(0..3)}?")
+        val program = input.getInstructions()
+        val toFactor = 2*2*19*11 + (program[21].B*22+program[23].B) + ((27*28+29)*30*14*32)
+        return divisorSum(toFactor).toString()
+    }
+
+    private fun divisorSum(n: Int) : Int {
+        val sqrt = isqrt(n)
+        var out = 1 + n
+
+        for (i in 2 until sqrt)
+            if (n % i == 0)
+                out += i + n/i
+
+        if (n % sqrt == 0)
+            out += sqrt
+
+        return out
+    }
+
+    private fun isqrt(n: Int) : Int {
+        var prev = n
+
+        while (true) {
+            val cur = (prev + n / prev) / 2
+            if (cur == prev) {
+                return cur
             }
-            Instruction(op, numbers[0], numbers[1], numbers[2])
+            prev = cur
         }
-        
-        
-        for (inst in program) {
-            when(inst.op) {
-                Operation.ADDR -> println("reg${inst.C} = reg${inst.A} + reg${inst.B}")
-                Operation.ADDI -> println("reg${inst.C} = reg${inst.A} + ${inst.B}")
-                Operation.MULR -> println("reg${inst.C} = reg${inst.A} * reg${inst.B}")
-                Operation.MULI -> println("reg${inst.C} = reg${inst.A} * ${inst.B}")
-                Operation.BANR -> println("reg${inst.C} = reg${inst.A} and reg${inst.B}")
-                Operation.BANI -> println("reg${inst.C} = reg${inst.A} and${inst.B}")
-                Operation.BORR -> println("reg${inst.C} = reg${inst.A} or reg${inst.B}")
-                Operation.BORI -> println("reg${inst.C} = reg${inst.A} or ${inst.B}")
-                Operation.SETR -> println("reg${inst.C} = reg${inst.A}")
-                Operation.SETI -> println("reg${inst.C} = ${inst.A}")
-                Operation.GTIR -> println("reg${inst.C} = if (${inst.A} > reg${inst.B}) 1 else 0")
-                Operation.GTRI -> println("reg${inst.C} if (reg${inst.A} >${inst.B}) 1 else 0")
-                Operation.GTRR -> println("reg${inst.C} if (reg${inst.A} > reg${inst.B}) 1 else 0")
-                Operation.EQIR -> println("reg${inst.C} if (${inst.A} == reg${inst.B}) 1 else 0")
-                Operation.EQRI -> println("reg${inst.C} if (reg${inst.A} ==${inst.B}) 1 else reg 0")
-                Operation.EQRR -> println("reg${inst.C} if (reg${inst.A} == reg${inst.B}) 1 else 0")
-            }
-        }*/
-
-        val reg2 = 10551377
-        var ret = 0
-
-        for (i in 1..reg2/2) {
-            if (reg2 % i == 0) {
-                ret += i
-                println(ret)
-            }
-        }
-
-        return ""
     }
 }
