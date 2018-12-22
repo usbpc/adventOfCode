ddfdc76d19b8b29ddee9871b72b5de280d9d2681
usbpc <usbpc@usbpc.xyz>
2018-12-22 09:22:02 +0100

    day 22 bad global is gone

--- src/main/kotlin/advent2018/Day22.kt
+++ src/main/kotlin/advent2018/Day22.kt
@@ -1,6 +1,5 @@
 package advent2018
 
-import org.omg.CORBA.Current
 import xyz.usbpc.aoc.Day
 import xyz.usbpc.aoc.inputgetter.AdventOfCode
 import java.lang.IllegalStateException
@@ -26,43 +25,38 @@ class Day22(override val adventOfCode: AdventOfCode) : Day {
         fun isValid() = this.x >= 0 && this.y >= 0
     }
 
-    private val erosionMap = mutableMapOf<Coord, BigInteger>()
-
-    private fun Coord.geologicIndex(depth: Int, goal: Coord) : BigInteger {
-        val out = if (this.x == 0 && this.y == 0) {
-            BigInteger.ZERO
+    private fun Coord.geologicIndex(depth: Int, goal: Coord, erosionMap: MutableMap<Coord, Long>) : Long {
+        return if (x == 0 && y == 0) {
+            0L
         } else if (this == goal) {
-            BigInteger.ZERO
-        } else if (this.y == 0) {
-            BigInteger.valueOf(this.x * 16807L)
-        } else if (this.x == 0) {
-            BigInteger.valueOf(this.y * 48271L)
+            0L
+        } else if (y == 0) {
+            x * 16807L
+        } else if (x == 0) {
+            y * 48271L
         } else {
-            this.up().erosionLevel(depth, goal) * this.left().erosionLevel(depth, goal)
+            up().erosionLevel(depth, goal, erosionMap) * left().erosionLevel(depth, goal, erosionMap)
         }
-
-        return out
     }
 
-    private fun Coord.erosionLevel(depth: Int, goal: Coord) : BigInteger {
+    private fun Coord.erosionLevel(depth: Int, goal: Coord, erosionMap: MutableMap<Coord, Long>) : Long {
         erosionMap[this]?.let { out ->
             return out
         }
 
-        val out = ((BigInteger.valueOf(depth.toLong()) + this.geologicIndex(depth, goal)) % BigInteger.valueOf(20183))
+        val out = (depth + this.geologicIndex(depth, goal, erosionMap)) % 20183
 
         erosionMap[this] = out
 
         return out
     }
 
-    private fun Coord.regionType(depth: Int, goal: Coord) : CaveType {
-        val erosionLevel = ((BigInteger.valueOf(depth.toLong()) + this.geologicIndex(depth, goal)) % BigInteger.valueOf(20183)).toLong() % 3
-        return when (erosionLevel) {
+    private fun Coord.regionType(depth: Int, goal: Coord, erosionMap: MutableMap<Coord, Long>) : CaveType {
+        return when (erosionLevel(depth, goal, erosionMap) % 3) {
             0L -> CaveType.ROCKY
             1L -> CaveType.WET
             2L -> CaveType.NARROW
-            else -> throw IllegalStateException("This can not happen! $erosionLevel $this $depth $goal")
+            else -> throw IllegalStateException("This can not happen!")
         }
     }
 
@@ -71,9 +65,11 @@ class Day22(override val adventOfCode: AdventOfCode) : Day {
         val target = Coord(input[1][0], input[1][1])
         var out = 0L
 
+        val erosionMap = mutableMapOf<Coord, Long>()
+
         for (y in 0..target.y) {
             for (x in 0..target.x) {
-                val curType = Coord(x, y).regionType(depth, target)
+                val curType = Coord(x, y).regionType(depth, target, erosionMap)
                 /*if ((x == 0 && y == 0) || target.y == y && target.x == x) {
                     print('X')
                 } else {
@@ -112,14 +108,17 @@ class Day22(override val adventOfCode: AdventOfCode) : Day {
     }
 
     override fun part2(): String {
-        val depth = input[0][0]
-        val target = Coord(input[1][0], input[1][1])
+        val depth = 510 //input[0][0]
+        val target = Coord(10, 10)//Coord(input[1][0], input[1][1])
 
         val queue = ArrayDeque<CurrentSituation>()
         val seen = mutableMapOf<Pair<Coord, Tool>, Int>()
 
         queue.add(CurrentSituation(Coord(0, 0), 0, Tool.TORCH))
         var minCost = Int.MAX_VALUE
+
+        val erosionMap = mutableMapOf<Coord, Long>()
+
         loop@while (queue.isNotEmpty()) {
             val cur = queue.poll()
             if (cur.cost >= minCost)
@@ -152,15 +151,15 @@ class Day22(override val adventOfCode: AdventOfCode) : Day {
             cur.pos.adjecent()
                     .filter(Coord::isValid)
                     .forEach { adj ->
-                        val regionType = adj.regionType(depth, target)
+                        val regionType = adj.regionType(depth, target, erosionMap)
                         if (cur.tool in regionType.validTools) {
                             queue.add(CurrentSituation(adj, cur.cost + 1, cur.tool))
                         } else {
-                            queue.add(CurrentSituation(adj, cur.cost + 1 + 7, cur.pos.regionType(depth, target).validTools.intersect(regionType.validTools).single()))
+                            queue.add(CurrentSituation(adj, cur.cost + 1 + 7, cur.pos.regionType(depth, target, erosionMap).validTools.intersect(regionType.validTools).single()))
                         }
                     }
         }
-        println(Coord(x=1, y=5).regionType(depth, target))
+
         return "" + minCost
     }
 }
