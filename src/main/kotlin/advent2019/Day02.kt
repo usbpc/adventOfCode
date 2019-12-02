54243ef86ba0361d372d891b61e9044e5e73c899
usbpc <usbpc@holm.dev>
2019-12-02 10:59:09 +0100

    2019 Day 02 refactored Intcode

--- src/main/kotlin/advent2019/Day02.kt
+++ src/main/kotlin/advent2019/Day02.kt
@@ -5,63 +5,41 @@ import xyz.usbpc.aoc.inputgetter.AdventOfCode
 
 class Day02(override val adventOfCode: AdventOfCode) : Day {
     override val day = 2
-    val input = adventOfCode.getInput(2019, day).split(",").map { it.toInt() }
-
-    fun MutableList<Int>.simulate() : Int {
-        var ip = 0
-
-        loop@while (true) {
-            //Load operators into registers
-            when (this[ip]) {
-                1 -> {
-                    this[this[ip+3]] = this[this[ip+1]] + this[this[ip+2]]
-                    ip += 4
-                }
-
-                2 -> {
-                    this[this[ip+3]] = this[this[ip+1]] * this[this[ip+2]]
-                    ip += 4
-                }
-
-                99 -> break@loop
-
-                else -> NotImplementedError("Opcode ${this[ip]} is not implemented yet!")
-            }
-        }
-
-        return this[0]
-    }
+    val input = Intcode.run { adventOfCode.getInput(2019, day).parse() }
 
     override fun part1() : Any {
-        val myInput = input.toMutableList()
+        Intcode.run {
+            val myInput = input.toMutableList()
 
-        myInput[1] = 12
-        myInput[2] = 2
+            myInput[1] = 12
+            myInput[2] = 2
 
-       return myInput.simulate()
+            return myInput.simulate()
+        }
     }
 
     override fun part2() : Any {
+        Intcode.run {
+            var noun = 0
+            var verb = 0
 
-        var noun = 0
-        var verb = 0
+            while (true) {
+                val myInput = input.toMutableList()
 
-        while (true) {
-            val myInput = input.toMutableList()
+                myInput[1] = noun
+                myInput[2] = verb
 
-            myInput[1] = noun
-            myInput[2] = verb
+                if (myInput.simulate() == 19690720)
+                    break
 
-            if (myInput.simulate() == 19690720)
-                break
-
-            noun++
-            if (noun == 100) {
-                noun = 0
-                verb++
+                noun++
+                if (noun == 100) {
+                    noun = 0
+                    verb++
+                }
             }
-        }
 
         return 100 * noun + verb
+        }
     }
 }
---
+++ src/main/kotlin/advent2019/Intcode.kt
@@ -0,0 +1,29 @@
+package advent2019
+
+object Intcode {
+    fun String.parse() : List<Int> = this.split(",").map { it.toInt() }
+
+    fun MutableList<Int>.simulate() : Int {
+        var ip = 0
+
+        loop@while (true) {
+            when (this[ip]) {
+                1 -> {
+                    this[this[ip+3]] = this[this[ip+1]] + this[this[ip+2]]
+                    ip += 4
+                }
+
+                2 -> {
+                    this[this[ip+3]] = this[this[ip+1]] * this[this[ip+2]]
+                    ip += 4
+                }
+
+                99 -> break@loop
+
+                else -> NotImplementedError("Opcode ${this[ip]} is not implemented yet!")
+            }
+        }
+
+        return this[0]
+    }
+}
