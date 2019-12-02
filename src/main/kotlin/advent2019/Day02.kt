e339145bc547547bc0e3de34f1de32de9a8b8c74
usbpc <usbpc@holm.dev>
2019-12-02 10:44:14 +0100

    2019 Day 02 cleaned up

--- src/main/kotlin/advent2019/Day02.kt
+++ src/main/kotlin/advent2019/Day02.kt
@@ -7,73 +7,55 @@ class Day02(override val adventOfCode: AdventOfCode) : Day {
     override val day = 2
     val input = adventOfCode.getInput(2019, day).split(",").map { it.toInt() }
 
+    fun MutableList<Int>.simulate() : Int {
+        var ip = 0
+
+        loop@while (true) {
+            //Load operators into registers
+            when (this[ip]) {
+                1 -> {
+                    this[this[ip+3]] = this[this[ip+1]] + this[this[ip+2]]
+                    ip += 4
+                }
+
+                2 -> {
+                    this[this[ip+3]] = this[this[ip+1]] * this[this[ip+2]]
+                    ip += 4
+                }
+
+                99 -> break@loop
+
+                else -> NotImplementedError("Opcode ${this[ip]} is not implemented yet!")
+            }
+        }
+
+        return this[0]
+    }
+
     override fun part1() : Any {
-        var myInput = input.toMutableList()
+        val myInput = input.toMutableList()
 
         myInput[1] = 12
         myInput[2] = 2
 
-        var pos = 0;
-        loop@for (i in 0 until myInput.size step 4) {
-            if (input[i] == 99 || i + 1 > input.size)
-                break
-
-            println(myInput)
-
-            val op1 = myInput[myInput[i + 1]]
-            val op2 = myInput[myInput[i + 2]]
-            val pos = myInput[i + 3]
-            val calc = when (myInput[i]) {
-                1 -> op1 + op2;
-                2 -> op1 * op2
-                99 -> break@loop
-                else -> {
-                    println(myInput[i])
-                    throw NotImplementedError()
-                }
-            }
-            myInput[pos] = calc
-        }
-
-       return myInput[0]
+       return myInput.simulate()
     }
 
     override fun part2() : Any {
-        val goal = 19690720
+
         var noun = 0
         var verb = 0
 
         while (true) {
-            var myInput = input.toMutableList()
+            val myInput = input.toMutableList()
 
             myInput[1] = noun
             myInput[2] = verb
 
-            loop@for (i in 0 until myInput.size step 4) {
-                if (input[i] == 99 || i + 1 > input.size)
-                    break
-
-                println(myInput)
-
-                val op1 = myInput[myInput[i + 1]]
-                val op2 = myInput[myInput[i + 2]]
-                val pos = myInput[i + 3]
-                val calc = when (myInput[i]) {
-                    1 -> op1 + op2;
-                    2 -> op1 * op2
-                    99 -> break@loop
-                    else -> {
-                        println(myInput[i])
-                        throw NotImplementedError()
-                    }
-                }
-                myInput[pos] = calc
-            }
-            println(myInput[0])
-            if (goal == myInput[0])
+            if (myInput.simulate() == 19690720)
                 break
-            noun++
 
+            noun++
             if (noun == 100) {
                 noun = 0
                 verb++
