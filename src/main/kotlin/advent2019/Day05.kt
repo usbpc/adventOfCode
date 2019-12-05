60e0e7ae57de0bc93fa2b1601e59c0135b0a92db
usbpc <usbpc@holm.dev>
2019-12-05 14:37:13 +0100

    2019 Day 05 Intcode improvment WIP

--- src/main/kotlin/advent2019/Day05.kt
+++ src/main/kotlin/advent2019/Day05.kt
@@ -1,21 +1,35 @@
 package advent2019
 
+import kotlinx.coroutines.runBlocking
 import xyz.usbpc.aoc.Day
 import xyz.usbpc.aoc.inputgetter.AdventOfCode
+import java.io.PipedInputStream
+import java.io.PipedOutputStream
 
 class Day05(override val adventOfCode: AdventOfCode) : Day {
     override val day = 5
     val input = adventOfCode.getInput(2019, day).split(",").map { it.toInt() }
 
+    fun something() = runBlocking {
+
+    }
+
     override fun part1() : Any {
-        /*val myInput = "3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99"
-                .split(",")
-                .map { it.toInt() }
-                .toMutableList()//
-        */
         val myInput = input.toMutableList()
 
-        return Intcode.run { myInput.simulate() }
+        val toVM = PipedOutputStream()
+        val vmIn = PipedInputStream(toVM, 255)
+
+        toVM.bufferedWriter().write("1\n")
+        toVM.flush()
+        println("I got this far!")
+
+        val vmOut = PipedOutputStream()
+        val fromVM = PipedInputStream(vmOut, 1024*255)
+
+        Intcode.run { myInput.simulate(vmIn, vmOut) }
+
+        return fromVM.reader().readLines().last()
     }
 
     override fun part2() : Any {
--- src/main/kotlin/advent2019/Intcode.kt
+++ src/main/kotlin/advent2019/Intcode.kt
@@ -1,8 +1,10 @@
 package advent2019
 
-object Intcode {
-    fun String.parse() : List<Int> = this.split(",").map { it.toInt() }
+import java.io.InputStream
+import java.io.OutputStream
 
+class Intcode(val state : MutableList<Int>) {
+    var ip: Int = 0
     fun MutableList<Int>.getRespectingMode(mode: Int, position: Int): Int {
         return when(mode) {
             0 -> { this[this[position]] }
@@ -10,79 +12,148 @@ object Intcode {
             else -> throw NotImplementedError("Mode ${mode} is not implemented yet.")
         }
     }
+    private operator fun get(index: Int) : Int {
+        var thing = 10
+        repeat(index) { thing *= 10 }
+        val mode = (state[ip] / thing) % 10
+        return when(mode) {
+            0 -> state[state[ip+index]]
+            1 -> state[ip+index]
+            else -> throw NotImplementedError("Mode ${mode} is nor implemented yet!")
+        }
+    }
 
-    fun MutableList<Int>.simulate() : List<Int> {
+    /**
+     * Steps forward one step.
+     */
+    fun step() : Boolean {
+        when (state[ip] % 100) {
+            1 -> {
+                state[state[ip+3]] = this[1] + this[2]
+                ip += 4
+            }
+
+            2 -> {
+                state[state[ip+3]] = this[1] * this[2]
+                ip += 4
+            }
+            3 -> {
+                //TODO get input
+                state[state[ip+1]] = 1
+                ip += 2
+            }
+            4 -> {
+                //println(getRespectingMode((this[ip]/100) % 10, ip+1))
+                println("VM Output: ${this[1]}")
+                ip += 2
+            }
+            5 -> {
+                if (this[1] != 0) {
+                    ip = this[2]
+                } else {
+                    ip += 3
+                }
+            }
+            6 -> {
+                if (this[1] == 0) {
+                    ip = this[2]
+                } else {
+                    ip += 3
+                }
+            }
+            7 -> {
+                if (this[1] < this[2]) {
+                    state[state[ip+3]] = 1
+                } else {
+                    state[state[ip+3]] = 0
+                }
+                ip += 4
+            }
+            8 -> {
+                if (this[1] == this[2]) {
+                    state[state[ip+3]] = 1
+                } else {
+                    state[state[ip+3]] = 0
+                }
+                ip += 4
+            }
+
+            99 -> return false
+
+            else -> NotImplementedError("Opcode ${state[ip]} is not implemented yet!")
+        }
+
+        return true
+    }
+
+    fun MutableList<Int>.simulate(input: InputStream? = null, output: OutputStream? = null) : List<Int> {
         //parameter mode
         // 0 = position mode
         // 1 = immediate mode
         //var mode = 0
         //Instruction pointer
-        var ip = 0
-        /*
+        val writer = output?.writer()
+        val reader = input?.bufferedReader()
 
-    Opcode 3 takes a single integer as input and saves it to the address given by its only parameter. For example, the instruction 3,50 would take an input value and store it at address 50.
-    Opcode 4 outputs the value of its only parameter. For example, the instruction 4,50 would output the value at address 50.
-
-         */
         loop@while (true) {
             //TODO parameter modes
-            when (this[ip] % 10) {
+            when (state[ip] % 100) {
                 1 -> {
-                    this[this[ip+3]] = getRespectingMode((this[ip]/100) % 10, ip+1) + getRespectingMode((this[ip]/1000) % 10, ip+2)
+                    state[state[ip+3]] = getRespectingMode((state[ip]/100) % 10, ip+1) + getRespectingMode((state[ip]/1000) % 10, ip+2)
                     ip += 4
                 }
 
                 2 -> {
-                    this[this[ip+3]] = getRespectingMode((this[ip]/100) % 10, ip+1) * getRespectingMode((this[ip]/1000) % 10, ip+2)
+                    state[state[ip+3]] = getRespectingMode((state[ip]/100) % 10, ip+1) * getRespectingMode((state[ip]/1000) % 10, ip+2)
                     ip += 4
                 }
                 3 -> {
                     //Input to this
-                    println("Bitte gib mir eine Zahl:")
-                    val thing : Int = readLine()!!.toInt()
-                    println("I've read: $thing")
-                    this[this[ip+1]] = thing
+                    writer?.write("Input: ")
+                    writer?.flush()
+                    state[state[ip+1]] = reader?.readLine()!!.toInt()
                     ip += 2
                 }
                 4 -> {
-                    //Output this
-                    println(getRespectingMode((this[ip]/100) % 10, ip+1))
+                    //println(getRespectingMode((this[ip]/100) % 10, ip+1))
+                    writer?.write("${getRespectingMode((state[ip]/100) % 10, ip+1)}\n")
+                    writer?.flush()
                     ip += 2
                 }
                 5 -> {
-                    if (getRespectingMode((this[ip]/100) % 10, ip+1) != 0) {
-                        ip = getRespectingMode((this[ip]/1000) % 10, ip+2)
+                    if (getRespectingMode((state[ip]/100) % 10, ip+1) != 0) {
+                        ip = getRespectingMode((state[ip]/1000) % 10, ip+2)
                     } else {
                         ip += 3
                     }
                 }
                 6 -> {
-                    if (getRespectingMode((this[ip]/100) % 10, ip+1) == 0) {
-                        ip = getRespectingMode((this[ip]/1000) % 10, ip+2)
+                    if (getRespectingMode((state[ip]/100) % 10, ip+1) == 0) {
+                        ip = getRespectingMode((state[ip]/1000) % 10, ip+2)
                     } else {
                         ip += 3
                     }
                 }
                 7 -> {
-                    if (getRespectingMode((this[ip]/100) % 10, ip+1) < getRespectingMode((this[ip]/1000) % 10, ip+2)) {
-                        this[this[ip+3]] = 1
+                    if (getRespectingMode((state[ip]/100) % 10, ip+1) < getRespectingMode((state[ip]/1000) % 10, ip+2)) {
+                        state[state[ip+3]] = 1
                     } else {
-                        this[this[ip+3]] = 0
+                        state[state[ip+3]] = 0
                     }
                     ip += 4
                 }
                 8 -> {
-                    if (getRespectingMode((this[ip]/100) % 10, ip+1) == getRespectingMode((this[ip]/1000) % 10, ip+2)) {
-                        this[this[ip+3]] = 1
+                    if (getRespectingMode((state[ip]/100) % 10, ip+1) == getRespectingMode((state[ip]/1000) % 10, ip+2)) {
+                        state[state[ip+3]] = 1
                     } else {
-                        this[this[ip+3]] = 0
+                        state[state[ip+3]] = 0
                     }
                     ip += 4
                 }
 
                 99 -> break@loop
 
-                else -> NotImplementedError("Opcode ${this[ip]} is not implemented yet!")
+                else -> NotImplementedError("Opcode ${state[ip]} is not implemented yet!")
             }
         }
 
